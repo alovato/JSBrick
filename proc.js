1097ed570be5072e69f11bd24e1f130051a3ee2c
Aaron Lovato <alovato@nmt.edu>
2010-11-01 21:43:14 -0600

    Merged changes to proc.js. Also implemented a couple of CP instructions.

--- proc.js
+++ proc.js
@@ -465,6 +465,34 @@ opcodes[0x9D] = function SBC_A_L() {
 	lastClock = 4;
 }
 
+opcodes[0xBF] = function CP_A() {
+    // Kind of a do-nothing operation
+    F |= flag_zero;
+    F |= flag_operation; 
+    lastclock = 4;
+}
+
+opcodes[0xB8] = function CP_B() {
+    if (B == A) {
+            F |= flag_zero;
+    }
+    else {    
+        F &= !flag_zero;
+    }
+    F != flag_operation; 
+    lastclock = 4;
+}
+
+opcodes[0xB9] = function CP_C() {
+    if (C == A) {
+            F |= flag_zero;
+    }
+    else {    
+        F &= !flag_zero;
+    }
+    F != flag_operation; 
+    lastclock = 4;
+}
 
 opcodes[0xAF] = function XOR_A() /*0xAF*/ { A ^= A; F=0; if(!(A & 0xFF)) F|=flag_zero; lastClock = 4; }
 
